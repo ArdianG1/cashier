de4ddcb8974020299f81cb76edb8a39410f5ca02
Ardian Syachrivano G <vanoginiting07@gmail.com>
2023-09-20 14:45:28 +0700

    Search Barang Service

--- src/test/java/com/smk/cashier/service/BarangService.java
+++ src/test/java/com/smk/cashier/service/BarangService.java
@@ -93,4 +93,9 @@ public class BarangService {
         barangList.add(barang);
         writeFile();
     }
+    public List<Barang> findbyname (String name) {
+        List<Barang> resultList = barangList.stream().filter(barang -> barang.getNamaBarang().startsWith(name)).toList();
+        return resultList;
+    }
+
 }
